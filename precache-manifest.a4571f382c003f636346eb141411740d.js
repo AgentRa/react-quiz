6565db97ab61780eefa57064295b6b4ee26ac19e
Aliaksandr Razhachkin <bubonik.san1993@gmail.com>
2020-05-09 02:36:30 +0300

    Init: upload working project to gh-pages

---
+++ precache-manifest.a4571f382c003f636346eb141411740d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deafe9393e59099b324d1dfd12a2d0bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d47ba6b96ee4f7e6868a",
+    "url": "./static/css/main.e99fc6cc.chunk.css"
+  },
+  {
+    "revision": "afd4f022a0fb43f2cb0e",
+    "url": "./static/js/2.122f523a.chunk.js"
+  },
+  {
+    "revision": "481514af0a7c51a9565f702ea3206008",
+    "url": "./static/js/2.122f523a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d47ba6b96ee4f7e6868a",
+    "url": "./static/js/main.f55290a8.chunk.js"
+  },
+  {
+    "revision": "2664a3b599cd121e6ee9",
+    "url": "./static/js/runtime-main.7fbc8434.js"
+  }
+]);
--- src/components/UI/Backdrop/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import classes from './index.module.scss'
-
-const Backdrop = props => <div className={classes.Backdrop} onClick={props.onClick} />
-
-export default Backdrop
